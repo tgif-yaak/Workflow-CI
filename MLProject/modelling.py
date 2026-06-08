27a4d7655c1d63361872acd876ea07a1c581cfb9
tgif-yaak <pradnyaliamaharani@gmail.com>
2026-06-08 18:19:34 +0700

    fix: resolve mlflow exception with nested run and remove experiment set

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,18 +6,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 def main():
-    # Bagian SQLite dihapus biar gak macet di server GitHub
-    mlflow.set_experiment("Eksperimen_Model_Tanah_Aliya")
+    # BARIS EXPERIMENT DIHAPUS/DIMATIKAN BIAR GAK BENTROK ID DI GITHUB ACTIONS
+    # mlflow.set_experiment("Eksperimen_Model_Tanah_Aliya")
 
     print("Memuat data...")
-    df = pd.read_csv("crop_preprocessed.csv")
+    df = pd.read_csv("crop_preprocessing.csv")
 
     X = df.drop(columns=['label'])
     y = df['label']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    with mlflow.start_run(run_name="RandomForest_Manual_Run"):
+    # TAMBAHKAN nested=True BIAR SINKRON SAMA RUN ID DARI SERVER GITHUB ACTIONS
+    with mlflow.start_run(run_name="RandomForest_Manual_Run", nested=True):
         print("Melatih model Random Forest...")
         
         n_estimators = 100
@@ -31,10 +32,11 @@ def main():
         mlflow.log_param("n_estimators", n_estimators)
         mlflow.log_metric("accuracy", acc)
         mlflow.sklearn.log_model(model, "model_tanah_aliya")
-        mlflow.sklearn.save_model(model, "my_model")
+        
+        # BARIS SAVE_MODEL DIHAPUS KARENA BIKIN PROSES RE-TRAINING MANDALIK EROR DI SERVER GITHUB
+        # mlflow.sklearn.save_model(model, "my_model")
         
         print(f"Model berhasil dilatih! Akurasi: {acc * 100:.2f}%")
 
 if __name__ == "__main__":
     main()
-    
